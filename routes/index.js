11fa429568a3f0ceeb0809d8f79bda90c620c9b4
joseejones13 <jose.jones@ontariotechu.net>
2025-11-07 16:19:57 -0500

    added images to the prjects, and the actual description of the projects

--- routes/index.js
+++ routes/index.js
@@ -24,9 +24,9 @@ router.get('/about', function(req, res, next) {
 router.get('/projects', function(req, res, next) {
   // example placeholder projects
   const projects = [
-    { title: "Project One", summary: "A short description of project one.", image: "/images/placeholder-1.svg" },
-    { title: "Project Two", summary: "A short description of project two.", image: "/images/placeholder-2.svg" },
-    { title: "Project Three", summary: "A short description of project three.", image: "/images/placeholder-3.svg" }
+    { title: "Project 1: Simple Online Store", summary: "Last year I got the chance to make a fake virtual store using python code from my intro to programming class. In this project we created a group of items that could be purchased, had credit card verification and an order tracking sheet.", image: "/images/project1.png" },
+    { title: "Project 2: Grades as a Determinant of Success Debate Project ", summary: "In my Writing and Critical Thinking class, we had a project to research the pros and cons of having grades being the determinant factor of students' grades. Through my research I realized that I am for having grades as an accurate depiction of students' success in the classroom.", image: "/images/project2.jpg" },
+    { title: "Project 3: Video on Buisness SDG's (Sustainable Development Goals)", summary: "For this project I grouped up with some other students and we picked an Indigenous business that was actively making an impact on the Canadian economy. Through this project we were able to pick some SDG's and explain how they are targeting/close to achieving these goals.", image: "/images/project3.png" }
   ];
   res.render('projects', { site: siteInfo, projects });
 });
